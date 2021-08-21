ac73c1d870578740e4c23523b8932a3ecf8ffbe2
naren <naren.kang@gmail.com>
2021-08-21 21:10:53 +0530

    starter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { EmpComponent } from './emp/emp.component';
+import { ContactusComponent } from './contactus/contactus.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    EmpComponent,
+    ContactusComponent
   ],
   imports: [
     BrowserModule,
